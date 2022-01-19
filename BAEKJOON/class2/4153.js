6fa2415e3638369e85ef75add51e533b753162c2
otterp012 <otterp012@gmail.com>
2022-01-19 11:13:05 +0900

    4153, correct

---
+++ BAEKJOON/class2/4153.js
@@ -0,0 +1,14 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map(items => items.split(' ').map(item => Number(item)));
+input.pop();
+
+function check(arr) {
+    arr = arr.map(item => item **2);
+
+    if(arr[0] === arr[1] + arr[2]) return 'right';
+    else if(arr[1] === arr[0] + arr[2]) return 'right';
+    else if(arr[2] === arr[0] + arr[1]) return 'right';
+    else return 'wrong'
+
+}
+
+input.forEach((item) => console.log(check(item)))
