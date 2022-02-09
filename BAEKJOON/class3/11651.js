03c603afc4913c641e1755c41337d61473dda39a
otterp012 <otterp012@gmail.com>
2022-02-09 21:07:46 +0900

    11651 correct

---
+++ BAEKJOON/class3/11651.js
@@ -0,0 +1,20 @@
+
+const input = require('fs').readFileSync('example.txt').toString().trim().split('\n');
+
+const N = input.shift();
+const coords = input.map((coord) => coord.split(' ').map(Number))
+
+
+
+coords.sort((a,b) => {
+    if(a[1] === b[1]) return a[0] - b[0];
+    return a[1]-b[1];
+})
+
+
+let answer = '';
+coords.forEach((coord) => {
+    answer += coord[0] + ' ' + coord[1] + '\n'
+})
+
+console.log(answer);
