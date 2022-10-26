16772721329d5307860c868a6cb0329814422c40
otterp012 <otterp012@gmail.com>
2022-10-26 11:17:22 +0900

    15652 correct

---
+++ BAEKJOON/BFS,DFS/15652.js
@@ -0,0 +1,27 @@
+const filePath = process.platform === "linux" ? "/dev/stdin" : "example.txt";
+const input = require("fs")
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split(" ")
+  .map(Number);
+
+const [N, M] = input;
+
+let answer = "";
+const output = [];
+
+const dfs = (cnt) => {
+  if (cnt === M) return (answer += output.join(" ") + "\n");
+
+  for (let i = 1; i <= N; i++) {
+    if (output[output.length - 1] > i) continue;
+    output.push(i);
+    dfs(cnt + 1);
+    output.pop();
+  }
+};
+
+dfs(0);
+
+console.log(answer);
